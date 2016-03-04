ff2b3b69c125fbce0c8cef1a426c3771bb96792c
Thomas Martin <ThomasMartin98@hotmail.com>
2016-03-04 13:37:53 +0000

    Added files via upload
    Solution for problem 14

---
+++ Problem.14.py
@@ -0,0 +1,24 @@
+#problem 14
+currentNumber=0
+currentChainLength=0
+largestChainLength=0
+lclStartNumber=0
+
+for i in range(2,1000000):
+    currentNumber=i
+    currentChainLength=0
+    while currentNumber!=1:
+        if currentNumber%2==0:
+            currentNumber=currentNumber/2
+        else:
+            currentNumber=(3*currentNumber)+1
+        currentChainLength+=1
+        if currentChainLength>largestChainLength:
+            largestChainLength=currentChainLength
+            lclStartNumber=i
+            print(lclStartNumber,largestChainLength)
+print(lclStartNumber)
+            
+        
+    
+    
